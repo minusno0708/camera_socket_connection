2326ed756ae237ee4171bc19d8e0380e9b9d7cc8
minusno0708 <naoyao1014@gmail.com>
2023-08-10 19:41:27 +0900

    [WIP]WebSocketの接続が出来ない

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(f *http.Request) bool {
+		return true
+	},
+}
+
+func handleWebsocket(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Printf("Failed to set websocket upgrade: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello Gin",
-		})
-	})
-	r.Run()
+	r.GET("/", handleWebsocket)
+	r.Run(":8080")
 }
 
